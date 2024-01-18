b37933fe3ae3514ed8d117f1eac8f8920f374fcc
rsarker1 <rahul.sarker@stonybrook.edu>
2024-01-17 23:04:30 -0500

    Toasts are now working. Working on auth

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,25 +1,30 @@
-"use client"
+'use client'
 
 import { FC, useState } from 'react'
 import { Button } from './ui/Button'
 import { cn } from '@/lib/utils'
 import { signIn } from 'next-auth/react'
 import { Icons } from './Icons'
+import { useToast } from '@/hooks/use-toast'
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
     
     const [isLoading, setIsLoading] = useState<boolean>(false)
-    //const {toast} = useToast()
+    const { toast } = useToast()
 
     const loginWithGoogle = async () => {
         setIsLoading(true)
 
         try {
-            await signIn('google')
+            await signIn("google")
         } catch(error) {
-
+            toast({
+                title: 'Error',
+                description: 'There was an error logging in with Google',
+                variant: 'destructive',
+            })
         } finally {
             setIsLoading(false)
         }
@@ -32,7 +37,7 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
             isLoading={isLoading}
             size='sm' 
             className='w-full'>
-                {isLoading ? null : <Icons.google className='h-4 w-4 mr-2'/>}
+            {isLoading ? null : <Icons.google className='h-4 w-4 mr-2'/>}
                 Google
             </Button> 
         </div>
