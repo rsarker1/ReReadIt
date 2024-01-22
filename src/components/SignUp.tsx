3878ce9cfc3376e4685e11f404b476f137d281e9
rsarker1 <rahul.sarker@stonybrook.edu>
2024-01-21 23:10:15 -0500

    Made modal for signup and finished auth

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -3,12 +3,12 @@ import { Icons } from "./Icons"
 import UserAuthForm from "./UserAuthForm"
 
 // Server component
-const SignIn = () => {
+const SignUp = () => {
   return (
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
         <div className="flex flex-col space-y-2 text-center">
             <Icons.logo className="mx-auto h-6 w-6" />
-            <h1 className="text-2xl font-semibold tracking-tight">Welcome Back</h1>
+            <h1 className="text-2xl font-semibold tracking-tight">Sign Up</h1>
             <p className="text-sm max-w-xs mx-auto">
                 By continuing, you are setting up a ReReaddit account and agree to our 
                 User Agreement and Privacy Policy. 
@@ -17,9 +17,9 @@ const SignIn = () => {
         {/* Sign in form */}
         <UserAuthForm />
         <p className="px-8 text-center text-sm text-zinc-700 ">
-            New to ReReaddit?{' '}
+            Already a ReReadditor? {' '}
             <Link 
-            href='/sign-up' 
+            href='/sign-in' 
             className="hover:text-zinc-800 text-sm underline underline-offset-4">
                 Sign Up
             </Link>
@@ -28,5 +28,5 @@ const SignIn = () => {
   )
 }
 
-export default SignIn
+export default SignUp
 
