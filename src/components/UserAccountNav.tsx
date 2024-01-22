1b91a1938809eff585c02f0556dfbb5ec14013f8
rsarker1 <rahul.sarker@stonybrook.edu>
2024-01-21 23:02:55 -0500

    Made intercepting auth modal and drop down menu for profile

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,9 +1,12 @@
 'use client'
 
-import { DropdownMenu, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
 import { User } from 'next-auth'
 import { FC } from 'react'
 import UserAvatar from './UserAvatar'
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent } from './ui/DropdownMenu'
+import { DropdownMenuItem, DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu'
+import Link from 'next/link'
+import { signOut } from 'next-auth/react'
 
 interface UserAccountNavProps {
   user: Pick<User, 'name' | 'image' | 'email'>
@@ -20,6 +23,45 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
             }}
         />
     </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <div className='flex items-center justify-start gap-2 p-2'>
+        <div className='flex flex-col space-y-1 leading-none'>
+          {user.name && <p className='font-medium'>{user.name}</p>}
+          {user.email && (
+            <p className='w-[200px] truncate text-sm text-zinc-700'>
+              {user.email}
+            </p>
+          )}
+        </div>
+      </div>
+
+      <DropdownMenuSeparator />
+      
+      <DropdownMenuItem asChild>
+            <Link href={'/'}> Feed </Link>
+      </DropdownMenuItem>
+
+      <DropdownMenuItem asChild>
+            <Link href={'/r/create'}> Create community </Link>
+      </DropdownMenuItem>
+
+      <DropdownMenuItem asChild>
+            <Link href={'/settings'}> Settings </Link>
+      </DropdownMenuItem>
+
+      <DropdownMenuSeparator />
+
+      <DropdownMenuItem onSelect={(event) => {
+        event.preventDefault()
+        signOut({
+          callbackUrl: `${window.location.origin}/sign-in`
+        })
+      }} className='cursor-pointer'>
+            Sign out
+      </DropdownMenuItem>
+
+    </DropdownMenuContent>
+    
   </DropdownMenu>
 }
 
