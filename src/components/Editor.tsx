cb49d59add3d3c50a18f3e6ffeba04a115967608
rsarker1 <rahul.sarker@stonybrook.edu>
2024-09-20 15:01:11 -0400

    Fixing uploadthing

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,43 +1,109 @@
 'use client'
 
-import { FC, useCallback, useRef } from 'react'
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
 import TextareaAutoSize from 'react-textarea-autosize'
 import { useForm } from 'react-hook-form'
 import { PostCreationRequest, PostValidator } from '@/lib/validators/post'
 import { zodResolver } from '@hookform/resolvers/zod'
 import type EditorJS from '@editorjs/editorjs'
+import { uploadFiles } from '@/uploadthing'
 
 interface EditorProps {
-    subredditId: string
+  subredditId: string
 }
 
 const Editor: FC<EditorProps> = ({ subredditId }) => {
-    const { register, handleSubmit, formState: { errors } } = useForm<PostCreationRequest>({
-        resolver: zodResolver(PostValidator),
-        defaultValues: {
-            subredditId,
-            title: '',
-            content: null
-        }
-    })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostCreationRequest>({
+    resolver: zodResolver(PostValidator),
+    defaultValues: {
+      subredditId,
+      title: '',
+      content: null,
+    },
+  })
 
-    const ref = useRef<EditorJS>()
+  const ref = useRef<EditorJS>()
+  const [isMounted, setIsMounted] = useState<boolean>(false)
 
-    const initializeEditor = useCallback(async () => {
-        const EditorJS = (await import('@editorjs/editorjs')).default
-        const Header = (await import('@editorjs/header')).default
-        const Embed = (await import('@editorjs/embed')).default
-        const Table = (await import('@editorjs/table')).default
-        const List = (await import('@editorjs/list')).default
-        const Code = (await import('@editorjs/code')).default
-        const LinkTool = (await import('@editorjs/link')).default
-        const InlineCode = (await import('@editorjs/inline-code')).default
-        const ImageTool = (await import('@editorjs/image')).default
-        //Not currently initialized
-        if(!ref.current) {
-            
-        }
-    }, [])
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsMounted(true)
+    }
+  }, [])
+
+  const initializeEditor = useCallback(async () => {
+    const EditorJS = (await import('@editorjs/editorjs')).default
+    const Header = (await import('@editorjs/header')).default
+    const Embed = (await import('@editorjs/embed')).default
+    const Table = (await import('@editorjs/table')).default
+    const List = (await import('@editorjs/list')).default
+    const Code = (await import('@editorjs/code')).default
+    const LinkTool = (await import('@editorjs/link')).default
+    const InlineCode = (await import('@editorjs/inline-code')).default
+    const ImageTool = (await import('@editorjs/image')).default
+    //Not currently initialized
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: 'editor',
+        onReady() {
+          ref.current = editor
+        },
+        placeholder: 'Type here to write your post',
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: {
+          header: Header,
+          linkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: '/api/link',
+            },
+          },
+          image: {
+            class: ImageTool,
+            config: {
+              uploader: {
+                async uploadByFile(file: File) {
+                  const [res] = await uploadFiles([file], 'imageUploader')
+
+                  return {
+                    success: 1,
+                    file: {
+                      url: res.fileUrl,
+                    },
+                  }
+                },
+              },
+            },
+          },
+          list: List,
+          code: Code,
+          inlineCode: InlineCode,
+          table: Table,
+          embed: Embed,
+        },
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor()
+
+      setTimeout(() => {
+  
+      })
+    }
+
+    if (isMounted) {
+      init()
+      return () => {}
+    }
+  }, [isMounted, initializeEditor])
 
   return (
     <div className='w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200'>
@@ -47,6 +113,8 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
             placeholder='Title'
             className='w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none'
           />
+
+          <div id='editor' className='min-h-[500px]'  />
         </div>
       </form>
     </div>
