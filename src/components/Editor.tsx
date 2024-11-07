92e61d4bd205599f53887bf1b995314f827a1d48
rsarker1 <rahul.sarker@stonybrook.edu>
2024-11-06 21:39:19 -0500

    Post creation

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -7,6 +7,11 @@ import { PostCreationRequest, PostValidator } from '@/lib/validators/post'
 import { zodResolver } from '@hookform/resolvers/zod'
 import type EditorJS from '@editorjs/editorjs'
 import { uploadFiles } from '@/uploadthing'
+import { toast } from '@/hooks/use-toast'
+import { PostPayload } from '@prisma/client'
+import { useMutation } from '@tanstack/react-query'
+import axios from 'axios'
+import { usePathname, useRouter } from 'next/navigation'
 
 interface EditorProps {
   subredditId: string
@@ -28,12 +33,9 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
 
   const ref = useRef<EditorJS>()
   const [isMounted, setIsMounted] = useState<boolean>(false)
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setIsMounted(true)
-    }
-  }, [])
+  const _titleRef = useRef<HTMLTextAreaElement>(null)
+  const pathname = usePathname()
+  const router = useRouter()
 
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import('@editorjs/editorjs')).default
@@ -45,7 +47,7 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
     const LinkTool = (await import('@editorjs/link')).default
     const InlineCode = (await import('@editorjs/inline-code')).default
     const ImageTool = (await import('@editorjs/image')).default
-    
+
     if (!ref.current) {
       const editor = new EditorJS({
         holder: 'editor',
@@ -69,7 +71,7 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
               uploader: {
                 async uploadByFile(file: File) {
                   const [res] = await uploadFiles('imageUploader', {
-                    files: [file]
+                    files: [file],
                   })
 
                   return {
@@ -93,30 +95,112 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
   }, [])
 
   useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsMounted(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (Object.keys(errors).length) {
+      for (const [_key, val] of Object.entries(errors)) {
+        toast({
+          title: 'Something went wrong',
+          description: (val as { message: string }).message,
+          variant: 'destructive',
+        })
+      }
+    }
+  }, [errors])
+
+  useEffect(() => {
     const init = async () => {
       await initializeEditor()
 
       setTimeout(() => {
-  
-      })
+        _titleRef.current?.focus()
+      }, 0)
     }
 
     if (isMounted) {
       init()
-      return () => {}
+      return () => {
+        ref.current?.destroy()
+        ref.current = undefined
+      }
     }
   }, [isMounted, initializeEditor])
 
+  const { mutate: createPost } = useMutation({
+    mutationFn: async ({
+      title,
+      content,
+      subredditId,
+    }: PostCreationRequest) => {
+      const payload: PostCreationRequest = {
+        title,
+        content,
+        subredditId,
+      }
+      const { data } = await axios.post('/api/subreddit/post/create', payload)
+      return data
+    },
+    onError: () => {
+      return toast({
+        title: 'Something went wrong',
+        description: 'Your post was not published, please try again later.',
+        variant: 'destructive',
+      })
+    },
+    onSuccess: () => {
+      const newPathname = pathname.split('/').slice(0, -1).join('/')
+      router.push(newPathname)
+      router.refresh()
+
+      return toast({
+        description: 'Your post has been published.',
+      })
+    },
+  })
+
+  async function onSubmit(data: PostCreationRequest) {
+    const blocks = await ref.current?.save()
+
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      subredditId,
+    }
+
+    createPost(payload)
+  }
+
+  if (!isMounted) {
+    return null
+  }
+
+  const { ref: titleRef, ...rest } = register('title')
+
   return (
     <div className='w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200'>
-      <form id='subreddit-post-form' className='w-fit' onSubmit={() => {}}>
+      <form
+        id='subreddit-post-form'
+        className='w-fit'
+        onSubmit={handleSubmit((e) => {})}
+      >
         <div className='prose porse-stone dark:prose-invert'>
           <TextareaAutoSize
+            ref={(e) => {
+              titleRef(e)
+
+              // @ts-ignore
+              _titleRef.current = e
+            }}
+            {...rest}
             placeholder='Title'
             className='w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none'
           />
 
-          <div id='editor' className='min-h-[500px]'  />
+          <div id='editor' className='min-h-[500px]' />
         </div>
       </form>
     </div>
